d2fe7012f9b74f80da4514a9a6dee7a7b6070c2c
Peter Kravets <petr.kravetss@gmail.com>
2017-07-07 18:45:37 +0300

    Add urls-data to post_icon.php

--- post_icon.php
+++ post_icon.php
@@ -2,9 +2,9 @@
 /*
 Plugin Name: Post Icon
 Description: Плагін добавляє іконку до заголовка статті
-Plugin URI:
+Plugin URI: https://github.com/Petekravets/-WordPress-Post-Icon-Plugin
 Author: Петро
-Author URI:
+Author URI: https://github.com/Petekravets
 */
 require __DIR__ . '/autoload.php';
 
