2520eee0c627e434fdbe31dfed88a906708df9c3
Peter Kravets <petr.kravetss@gmail.com>
2017-07-08 09:56:46 +0300

    Fixed autoload.php

--- autoload.php
+++ autoload.php
@@ -1,6 +1,16 @@
 <?php
-spl_autoload_register( 'autoloader' );
-function autoloader($class)
+
+spl_autoload_register('autoloader_func');
+function autoloader_func($class)
 {
-    require __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
+    if($class == 'WP_Filesystem_direct') {
+        return;
+    }
+    $plugin_directory = dirname(__FILE__) . '/';
+    $path = str_replace('\\', '/', $class) . '.php';
+
+    if ( file_exists( $plugin_directory.$path ) ) {
+        require_once $plugin_directory.$path;
+    }
+
 }
